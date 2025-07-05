e1fdffa31a6763606638400ff261fceff31a5224
Juan Manuel de los Santos <83610825+juanma-dls@users.noreply.github.com>
2025-07-05 03:13:08 -0300

    Feature/fcd search products (#2)
    * fix: se fijan las dependencias
    
    * feat: se configura imagen de docker para fcd-search-products
    
    * feat: se crea facade para el search de productos

---
+++ fcd-search-products/src/routes/productRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { searchProductController } from "../controllers/searchProductController";
+
+const router = Router();
+
+router.get("/products-search/", searchProductController);
+
+
+export default router;
---
+++ fcd-search-products/src/server/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import router from "../routes";
+import morgan from 'morgan';
+
+const server = express();
+
+server.use(express.json());
+server.use(morgan('combined'));
+
+server.use("/", router);
+
+export default server;
