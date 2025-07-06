900b6acbdff542ae39fd6649c0ad4310f076d78f
juanma-dls <juanmadls6@gmail.com>
2025-07-06 19:29:31 -0300

    feat: se agrega manejador de errores para int-products-ms

---
+++ int-products-ms/src/utils/middlewares/errorHandler.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import CustomError from "../errors/customError";
+
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => {
+
+  console.log(err);
+
+  if (err instanceof CustomError) {
+    res.status(err.statusCode).json({ errors: err.serializeErrors() });
+    return;
+  }
+
+  res.status(500).json({
+    errors: [{ message: "Internal Server Error" }]
+  });
+  next(err);
+}
+
+export default errorHandler;
