9f6f005c4bac2f6411661ad2120eee2ac2bf6749
juanma-dls <juanmadls6@gmail.com>
2025-07-06 19:26:47 -0300

    feat: se agrega manejador de errores y validador de request

--- bff-search-products/src/services/searchProductService.ts
+++ bff-search-products/src/services/searchProductService.ts
@@ -1,17 +1,29 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { environment } from "../config/environment";
 import { Request } from "express";
 import { parsePath } from "../utils/functions";
+import CustomError from "../utils/errors/customError";
 
-export const searchProductService = async (req: Request) => {
-  const parseUrlPath = parsePath(environment.FCD_SEARCH_PRODUCTS_PATH, {
-    req
-  });
-  const url = `${environment.FCD_SEARCH_PRODUCTS_URL}${parseUrlPath}`;
-  
-  const { data } = await axios.get(url, {
-    params: req.query,
-    timeout: environment.TIMEOUT,
-  });
-  return data || [];
+export const searchProductService = async (req: Request ) => {
+  try {
+    const parseUrlPath = parsePath(environment.FCD_SEARCH_PRODUCTS_PATH, {
+      req
+    });
+    const url = `${environment.FCD_SEARCH_PRODUCTS_URL}${parseUrlPath}`;
+    const headers = req.headers
+
+    const { data } = await axios.get(url, {
+      headers,
+      params: req.query,
+      timeout: environment.TIMEOUT,
+    });
+
+    return data || [];
+    
+  } catch (error: any) {
+    const status = error.response?.status || 500;
+    const message = error.response?.data?.errors?.[0]?.message || error.message || 'Error';
+
+    throw new CustomError(message, status);
+  };
 }
