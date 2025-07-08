e346a7693f677da46af37e63706910dc7b94a908
juanma-dls <juanmadls6@gmail.com>
2025-07-08 16:03:04 -0300

    fix: correccion de envio de params para el service de busqueda en bbf-products

--- bff-products/src/services/searchProductService.ts
+++ bff-products/src/services/searchProductService.ts
@@ -1,15 +1,12 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { environment } from "../config/environment";
 import { Request } from "express";
-import { parsePath } from "../utils/functions";
 import CustomError from "../utils/errors/customError";
+import { parseError } from "../helpers/parseError";
 
 export const searchProductService = async (req: Request ) => {
   try {
-    const parseUrlPath = parsePath(environment.FCD_SEARCH_PRODUCTS_PATH, {
-      req
-    });
-    const url = `${environment.FCD_SEARCH_PRODUCTS_URL}${parseUrlPath}`;
+    const url = `${environment.FCD_SEARCH_PRODUCTS_URL}${environment.FCD_SEARCH_PRODUCTS_PATH}`;
     const headers = req.headers
 
     const { data } = await axios.get(url, {
@@ -18,12 +15,14 @@ export const searchProductService = async (req: Request ) => {
       timeout: environment.TIMEOUT,
     });
 
-    return data || [];
+    if (data.items && data.items.length > 0) {
+      return data;
+    } else {
+      throw new CustomError("Products not found", 404);
+    }
     
-  } catch (error: any) {
-    const status = error.response?.status || 500;
-    const message = error.response?.data?.errors?.[0]?.message || error.message || 'Error';
-
+  } catch (error: unknown) {
+    const { status, message } = parseError(error, "Error while fetching products", 500);
     throw new CustomError(message, status);
   }
 }
