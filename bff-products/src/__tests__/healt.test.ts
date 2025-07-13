c27486e64c9ec2a31d981093ffced7dc0db2615b
juanma-dls <juanmadls6@gmail.com>
2025-07-13 16:46:17 -0300

    test: prueba husky en bff-products

--- bff-products/src/__tests__/healt.test.ts
+++ bff-products/src/__tests__/healt.test.ts
@@ -4,7 +4,7 @@ import server from "../server";
 describe("GET /health", () => {
   it("should return status OK", async () => {
     const res = await request(server).get("/health");
-    expect(res.status).toBe(200);
+    expect(res.status).toBe(400);
     expect(res.body.status).toMatch(/OK/i);
   });
 });
