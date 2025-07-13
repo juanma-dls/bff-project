9c189641e40b11951d0da4ba176d7c88706acb7c
juanma-dls <juanmadls6@gmail.com>
2025-07-13 15:49:36 -0300

    fix: corrijo tipo de logger en app.ts en fcd-products

--- fcd-products/src/app.ts
+++ fcd-products/src/app.ts
@@ -3,5 +3,5 @@ import server from "./server/index";
 import { logger } from "./utils/logger";
 
 server.listen(environment.PORT, () => {
-  logger.error(`Server is running on port ${environment.PORT}`);
+  logger.info(`Server is running on port ${environment.PORT}`);
 });
